14239206977c9cb05af003c5d7c4b52b81e48b2c
Polyanna Meira <polyannaxam@gmail.com>
2025-07-10 13:20:25 +0200

    fix: use DATABASE_URL for production

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,24 @@
- const configDatabase = {
-    dialect: 'postgres',
-    host:'localhost',
-    port: '5434',
-    database: 'Devburger',
-    username: 'postgres',
-    password: 'postgres',
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoreAll: true,
-},
-}
-export default configDatabase
+const configDatabase = {
+  dialect: 'postgres',
+  url: process.env.DATABASE_URL || undefined,
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '5434',
+  database: process.env.DB_NAME || 'Devburger',
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+  dialectOptions: process.env.DATABASE_URL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoreAll: true,
+  },
+};
+
+export default configDatabase;
